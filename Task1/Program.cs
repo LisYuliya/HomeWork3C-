c1d91ae448ffcc78d2b6479c2c93eedca59c84ce
Yuliya <8avokado8@gmail.com>
2022-10-25 18:17:16 +0600

    Done

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Задача 19
+
+Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+
+
+void Palindrom(string num)
+{
+    for (int i = 0; i < num.Length / 2; i++)
+    {
+        if (num[i] == num[num.Length - i - 1])
+
+            Console.WriteLine("Это число является полиндромом");
+
+        else
+
+            Console.WriteLine("Это число не является полиндромом");
+    }
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+
+Palindrom(Console.ReadLine()!);
---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Дополнительно.
+Напишите программу, которая принимает на вход число (N) и выдает таблицу простых чисел от 1 до N.
+11 -> 2, 3, 5 , 7, 11
+*/
+
+bool Simple = true;
+Console.WriteLine("Введите число, до которого нужно узнать все простые числа : ");
+int num = Convert.ToInt32(Console.ReadLine());
+for (int count = 2; count <= num; count++)
+{
+    for (int i = 2; i <= (count / 2) ; i++)
+    {
+        if (count % i != 0) Simple = true;
+        else { Simple = false; break; }
+    }
+    if (Simple == true) Console.WriteLine(count);
+}
